13534b33b2de0c07f3af7efe9537e705367b7160
Douglas Wissett Walker <douglas.wissett@gmail.com>
2020-05-31 19:56:12 +0100

    populate brand/subbrand & sort by sourceVolumeMetric

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import {
 } from './components';
 import pmq from './json/PMQ.json';
 
-console.log('pmq: ', pmq);
 
 function App() {
+  let sorted;
+  const sortBySourceVolumeMetric = () => {
+    sorted = pmq.sort((a, b) => a.SourceVolumeMetric - b.SourceVolumeMetric);
+  }
+
+  const data = sorted || pmq;
+
   return (
     <div className="App">
       <div className="container">
         <h1>PMQ</h1>
-        <ul>
+        <div>
+          <button onClick={sortBySourceVolumeMetric}>
+            Sort by SourceVolumeMetric
+          </button>
+        </div>
+        <ul className="row-list">
           {
-            pmq.map(item => (
+            data.map(item => (
               <PMQItem key={item.ID} item={item} />
             ))
           }
--- src/components/PMQItem.js
+++ src/components/PMQItem.js
@@ -1,27 +1,23 @@
 import React, { useState } from 'react';
 import brands from '../json/Brands.json';
 import subBrands from '../json/SubBrands.json';
-console.log('brands: ', brands);
-console.log('subBrands: ', subBrands);
+
 
 export default (props) => {
-  const [brand, setBrand] = useState(brands[0].BrandCode);
+  const [brand, setBrand] = useState('');
 
   const handleBrandCodeSubmit = (e) => setBrand(e.target.value);
 
   const formatSubBrands = () => {
-    console.log('format sub brand with brand ', brand);
-
-    console.log('formatted: ', subBrands.filter(b => b.BrandCode === brand));
-
-    return subBrands.filter(b => b.BrandCode === brand).map(item => (
-      <option key={item.ID} value={item.BrandCode}>{item.BrandName}</option>
+    return subBrands.filter(b => b.BrandCode.toString() === brand.toString()).map(item => (
+      <option key={item.ID} value={item.BrandCode}>{item.IndividualBrandVariantName}</option>
     ));
   }
 
   return (
-    <li>
+    <li className="row-item">
       <h2>{ props.item.BrandName }</h2>
+      <label className="row-label">Brand </label>
       <select
         name="brands"
         id="brands"
@@ -35,11 +31,12 @@ export default (props) => {
         }
       </select>
       <br />
-      <select name="brands" id="brands">
+      <label className="row-label">Subbrand </label>
+      <select
+        name="subBrands"
+        id="subBrands">
         { formatSubBrands() }
       </select>
-      <br />
-      <button onClick={handleBrandCodeSubmit}>Override</button>
     </li>
   );
 };
